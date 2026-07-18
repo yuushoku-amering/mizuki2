d8f500bbe849ac858a8ec6743f0e93f4734428d2
優色雨铃 <2330704234@qq.com>
2026-07-18 18:18:50 +0800

    初始化 Mizuki 博客 - 優色雨铃（yooshoku AmeRing の 资料库）

---
+++ src/config/profileConfig.ts
@@ -0,0 +1,39 @@
+import type { ProfileConfig } from "../types/config";
+
+// 个人资料配置
+export const profileConfig: ProfileConfig = {
+	avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
+	name: "優色雨铃（yuushoku amering）",
+	bio: "只是想唱些无济于事的歌",
+	typewriter: {
+		enable: true, // 启用个人简介打字机效果
+		speed: 80, // 打字速度（毫秒）
+	},
+	links: [
+		{
+			name: "Bilibili社交账号",
+			icon: "fa7-brands:bilibili",
+			url: "https://space.bilibili.com/284654879",
+		},
+		{
+			name: "Gitee",
+			icon: "mdi:git",
+			url: "https://gitee.com/matsuzakayuki",
+		},
+		{
+			name: "GitHub",
+			icon: "fa7-brands:github",
+			url: "https://github.com/matsuzaka-yuki",
+		},
+		{
+			name: "Codeberg",
+			icon: "simple-icons:codeberg",
+			url: "https://codeberg.org",
+		},
+		{
+			name: "Discord",
+			icon: "fa7-brands:discord",
+			url: "https://discord.gg/MqW6TcQtVM",
+		},
+	],
+};
