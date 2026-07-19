2ed9a5fc8da6f828e8413aa4a78a05e08dc981bc
優色雨铃 <2330704234@qq.com>
2026-07-19 19:26:30 +0800

    更换BGM为しゃろう - You and Me

--- src/config/musicConfig.ts
+++ src/config/musicConfig.ts
@@ -5,10 +5,10 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 	enable: true, // 启用音乐播放器功能
 	showFloatingPlayer: true, // 显示悬浮播放器 UI
 	floatingEntryMode: "fab", // 悬浮入口模式："default" 为独立悬浮播放器，"fab" 为集成到通用 FAB 组
-	mode: "local", // 音乐播放器模式，可选 "local" 或 "meting"
+	mode: "meting", // 音乐播放器模式，可选 "local" 或 "meting"
 	meting_api:
 		"https://meting.mysqil.com/api?server=:server&type=:type&id=:id&auth=:auth&r=:r", // Meting API 地址
-	id: "14164869977", // 歌单ID
+	id: "1831400558", // 歌单ID / 歌曲ID：しゃろう - You and Me
 	server: "netease", // 音乐源服务器。有的meting的api源支持更多平台,一般来说,netease=网易云音乐, tencent=QQ音乐, kugou=酷狗音乐, xiami=虾米音乐, baidu=百度音乐
-	type: "playlist", // 播单类型
+	type: "song", // 播单类型：song=单曲, playlist=歌单
 };
